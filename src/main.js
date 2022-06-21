8c3979774aa964eb0e785a0feb2164f79d8e25c5
oral0717 <807910824@qq.com>
2022-06-21 16:49:01 +0800

    路径别名

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './assets/styles/reset.css'
-import './assets/styles/border.css'
+import 'styles/reset.css'
+import 'styles/border.css'
 import fastClick from 'fastclick'
 fastClick.attach(document.body)
 
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
+const path = require('path')
 const { defineConfig } = require('@vue/cli-service')
+
+function resolve (dir) {
+  return path.resolve(__dirname, dir)
+}
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 1874,
+    proxy: {}
+  },
+  configureWebpack: {
+    resolve: {
+      extensions: ['.js', '.vue', '.json'],
+      alias: {
+        // 'vue$': 'vue/dist/vue.esm.js',
+        '@': resolve('src'),
+        'styles': resolve('src/assets/styles'),
+        'common': resolve('src/common'),
+      }
+    }
+  }
 })
