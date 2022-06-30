2fcd2140aef93be68f8c5ce9e2e54163b82994e6
oral0717 <807910824@qq.com>
2022-06-30 17:32:00 +0800

    update

---
+++ src/pages/detail/Detail.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="">
+    <div class=""></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DetailPage"
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
